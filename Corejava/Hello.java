b5ca129dea713e12bc43289a09719401941beab9
Aditya-ingole21 <aditayingole113@gmail.com>
2025-07-18 17:52:38 +0000

    initial commit

---
+++ Corejava/Hello.java
@@ -0,0 +1,10 @@
+
+
+class Hello {
+
+public static void main(String [] args ){
+System.out.println("Aditya B-Tech");
+System.out.println("java fs is one of the best skill");
+}
+
+}
---
+++ Corejava/Two.java
@@ -0,0 +1,26 @@
+class Two {
+static {
+System.out.println("static block1 says I am first");
+}
+static {
+System.out.println("static block2 says I am first");
+}
+
+public static void main(String [] args ){
+
+System.out.println("main() block says I am first");
+System.out.println("Aditya B-Tech");
+System.out.println("java fs is one of the best skill");
+}
+static {
+System.out.println("static block3 says I am first");
+}
+static {
+System.out.println("static block4 says I am first");
+}
+}
+
+
+
+
+
